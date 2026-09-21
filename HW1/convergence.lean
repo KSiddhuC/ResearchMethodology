6d0cd0cb967476f2011670d09e327fb7d5a622d0
KSiddhuC <kscsiddhu31@gmail.com>
2026-09-21 15:27:16 +0530

    cm2

--- HW1/convergence.lean
+++ HW1/convergence.lean
@@ -3,8 +3,8 @@ import Mathlib
 open Filter Topology
 
 theorem one_div_nat_tends_to_zero:
-  tendsto (fun n : ℕ => (1:ℝ)/(n:ℝ)) atTop (𝓝 0) := by
+  Tendsto (fun n : ℕ => (1 : ℝ)/(n : ℝ)) atTop (𝓝 0) := by
   simpa [one_div, Function.comp_def] using
   (tendsto_inv_atTop_zero.comp
   (tendsto_natCast_atTop_atTop :
-    tendsto (fun n : ℕ => (n:ℝ)) atTop atTop))
+    Tendsto (fun n : ℕ => (n:ℝ)) atTop atTop))
